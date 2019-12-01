a34fa0dba934824a6398e1a378ceb449b020299f
Sung Soo (Anthony) Cho <sc80@rice.edu>
2019-12-01 05:29:05 -0600

    Commented out use of cors to patch connection error

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static('dist'));
 }
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
