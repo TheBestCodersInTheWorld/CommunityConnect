2e4cf2ef749252c1f1b0bbdf2796b81ffc27992c
Sung Soo (Anthony) Cho <sc80@rice.edu>
2019-12-01 05:20:37 -0600

    Added webpack, babel, and other modules and structures necessary to run entire app on a single dyno by serving client as a static asset

--- server/server.js
+++ server/server.js
@@ -8,7 +8,21 @@ var mongoose = require('mongoose');
 var models = require('./models.js');
 var FoodSubmission = models.FoodSubmission;
 
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('../webpack.dev.js');
+
 mongoose.connect(require('./connection.js'), { useFindAndModify: false });
+
+app.use(express.static('public'));
+if (process.env.NODE_ENV === 'development') {
+  // Setup Webpack for development
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler));
+} else {
+  // Static serve the dist/ folder in production
+  app.use(express.static('dist'));
+}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -36,8 +50,8 @@ app.get('/fnb_get', (req, res) => {
 
 const port = process.env.PORT || 8000;
 
-// remember to run webpack or this path wont find the files needed for the bundle
-app.use(express.static(path.resolve(__dirname, '../public/')))
+// // remember to run webpack or this path wont find the files needed for the bundle
+// app.use(express.static(path.resolve(__dirname, '../public/')))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
