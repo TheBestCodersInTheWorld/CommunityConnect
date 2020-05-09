89175781b5a3d87a02d8f7fc091ef000b3886613
Dalia <DSandberg@Dalias-new-Mac.local>
2020-05-09 16:23:42 -0500

    Added an email in the about page

--- src/about/About.js
+++ src/about/About.js
@@ -11,6 +11,7 @@ class About extends React.Component {
                 Welcome to the Friday Volunteering FNB website, created by Dalia/Ben Sandberg. 
                 This web site can be used for signing up to bring food and other items such as plates or utensils for volunteering
                 on friday nights to feed homeless people, sponsored by the organization Food Not Bombs.
+                If you would like to change or edit a submission, contact fnbcommunityconnecthelp@gmail.com
             </div>
         )
     }
